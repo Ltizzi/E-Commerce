f1121a031ecf93d8b8decb4a75a0e5c0e90a4e3f
Leonardo <terlizzileonardo@gmail.com>
2023-05-11 19:34:06 -0300

    added admin dashboard component and new product modal component to give the admin user the ability to create a new product and add it to the catalogue

--- Web/src/app/app-routing.module.ts
+++ Web/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LandingComponent } from './components/layout/landing/landing.component'
 import { ProductListComponent } from './components/product/product-list/product-list.component';
 import { CartComponent } from './components/layout/cart/cart.component';
 import { ProductPageComponent } from './components/product/product-page/product-page.component';
+import { AdminDashboardComponent } from './components/admin/admin-dashboard/admin-dashboard.component';
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'products', component: ProductListComponent },
   { path: 'cart', component: CartComponent },
   { path: 'product', component: ProductPageComponent },
+  { path: 'admin', component: AdminDashboardComponent },
 ];
 
 @NgModule({

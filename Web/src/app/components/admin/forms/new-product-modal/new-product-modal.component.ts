4e6afe9f520c48100ea5e983de95868b2c0c3dad
Leonardo <terlizzileonardo@gmail.com>
2023-05-12 11:57:10 -0300

    added form validation to new product modal. Added the ability to add image urls to the new product. Added the gallery of more than one image in product page.

--- Web/src/app/components/admin/forms/new-product-modal/new-product-modal.component.ts
+++ Web/src/app/components/admin/forms/new-product-modal/new-product-modal.component.ts
@@ -8,7 +8,6 @@ import {
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ProductTypeService } from 'src/app/services/product-type.service';
 import { ProductService } from 'src/app/services/product.service';
-import { ModalService } from 'src/app/services/ui/modal.service';
 import { Product } from 'src/common/models/product';
 import { ProductType } from 'src/common/models/type';
 
@@ -29,24 +28,68 @@ export class NewProductModalComponent {
     ]),
     brand: new FormControl('', [
       Validators.required,
-      Validators.min(3),
+      Validators.minLength(3),
       Validators.maxLength(20),
     ]),
     price: new FormControl('', [Validators.required, Validators.min(0.1)]),
     about: new FormControl('', [
       Validators.required,
       Validators.minLength(3),
-      Validators.maxLength(200),
+      Validators.maxLength(1000),
     ]),
-    imageUrl: new FormControl(''),
-    prod_type: new FormControl(''),
+    // imageUrl: new FormControl(''),
+    prod_type: new FormControl('', Validators.required),
   });
 
   private element: any;
 
   types: Array<ProductType> = [];
 
+  urls: Array<string> = [];
+  url: string = '';
+
   successOperation: boolean = false;
+
+  errorMessages = {
+    name: [
+      { type: 'required', message: 'Product name is required' },
+      {
+        type: 'minlength',
+        message: 'Product name requires at least 4 characters',
+      },
+      {
+        type: 'maxlength',
+        message: 'Product name has a maximum of 25 characters',
+      },
+    ],
+    brand: [
+      { type: 'required', message: 'Brand name is required' },
+      {
+        type: 'minlength',
+        message: 'Brand name requires at least 3 characters',
+      },
+      {
+        type: 'maxlength',
+        message: 'Brand name has a maximum of 20 characters',
+      },
+    ],
+    price: [
+      { type: 'required', message: 'Price is required' },
+      { type: 'min', message: "Price can't be lower than $0.1" },
+    ],
+    about: [
+      { type: 'required', message: 'Product description is required' },
+      {
+        type: 'minlength',
+        message: 'Description requires at least 3 characters',
+      },
+      {
+        type: 'maxlength',
+        message: 'Description has a maximum of 1000 characters',
+      },
+    ],
+    prod_type: [{ type: 'required', message: 'Product type is required' }],
+  };
 
   constructor(
     private el: ElementRef,
@@ -64,6 +107,7 @@ export class NewProductModalComponent {
       this.types = data;
     });
     this.successOperation = false;
+    this.urls = [];
   }
   ngOnDestroy() {
     this.element.remove();
@@ -82,7 +126,7 @@ export class NewProductModalComponent {
         brand: this.newProductForm.value.brand as string,
         price: parseInt(this.newProductForm.value.price as string),
         about: this.newProductForm.value.about as string,
-        imageUrl: [],
+        imageUrl: this.urls,
         prod_type: type as ProductType,
       };
       console.log(newProduct);
@@ -101,5 +145,12 @@ export class NewProductModalComponent {
     // this.element.style.display = 'none';
     this.show = false;
     this.closeModal.emit(this.show);
+  }
+
+  addUrl(url: string) {
+    console.log(url);
+    this.urls.push(url);
+    console.log(this.urls);
+    this.url = '';
   }
 }

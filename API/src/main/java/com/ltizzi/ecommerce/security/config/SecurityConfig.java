038bbbae10042d1c4dacc4a150902ba7e209bfd7
Leonardo <terlizzileonardo@gmail.com>
2024-01-17 19:45:15 -0300

    refactorized cors configuration.

--- API/src/main/java/com/ltizzi/ecommerce/security/config/SecurityConfig.java
+++ API/src/main/java/com/ltizzi/ecommerce/security/config/SecurityConfig.java
@@ -1,14 +1,21 @@
 package com.ltizzi.ecommerce.security.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * @author Leonardo Terlizzi
@@ -20,10 +27,28 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.cors(Customizer.withDefaults()).csrf(csrf -> csrf.disable())
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setExposedHeaders(Arrays.asList("Authorization"));
+                        config.setMaxAge(3600L);
+                        return config;
+                    }
+                }))
                 .authorizeHttpRequests(authorize ->
                         //authenticated() para oauth
                         authorize.anyRequest().permitAll()).oauth2Login(); //Customizer.withDefaults()
+
+//        http.cors(Customizer.withDefaults()).csrf(csrf -> csrf.disable())
+//                .authorizeHttpRequests(authorize ->
+//                        //authenticated() para oauth
+//                        authorize.anyRequest().permitAll()).oauth2Login(); //Customizer.withDefaults()
 
         return http.build();
     }

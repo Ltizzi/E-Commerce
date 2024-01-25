7c66d27c9b703530dd3065034f6b324f7a09b0ee
Leonardo <terlizzileonardo@gmail.com>
2024-01-24 21:29:07 -0300

    added services with crud method for all entities

---
+++ Server/src/services/StockEntryService.ts
@@ -0,0 +1,92 @@
+import { AppDataSource } from "../data-source";
+import { StockEntity } from "../entities/StockEntity";
+import { StockEntryEntity } from "../entities/StockEntryEntity";
+import { StockEntry } from "../models/StockEntry";
+
+export class StockEntryService {
+  private entryRepo = AppDataSource.getRepository(StockEntryEntity);
+
+  private stockRepo = AppDataSource.getRepository(StockEntity);
+
+  async getAllEntries(): Promise<Array<StockEntryEntity>> {
+    return await this.entryRepo
+      .createQueryBuilder("entry")
+      .where({ soft_delete: false })
+      .orderBy("entry.entry.id", "ASC")
+      .getMany();
+  }
+
+  async getEntriesWithPagination(
+    page: number,
+    pageSize: number
+  ): Promise<Array<StockEntryEntity>> {
+    const skip = (page - 1) * pageSize;
+    return await this.entryRepo
+      .createQueryBuilder("entry")
+      .where({ soft_delete: false })
+      .orderBy("entry.entry_id", "ASC")
+      .skip(skip)
+      .take(pageSize)
+      .getMany();
+  }
+
+  async countEntries(): Promise<number> {
+    return await this.entryRepo.count();
+  }
+
+  async getEntryById(id: number): Promise<StockEntryEntity | null> {
+    return await this.entryRepo.findOneBy({ entry_id: id, soft_delete: false });
+  }
+
+  async saveEntry(entry: StockEntry): Promise<StockEntryEntity | null> {
+    const stock: StockEntity | null = await this.getStockById(
+      entry.stock.stock_id
+    );
+    if (stock) {
+      stock.cantidad = stock?.cantidad + entry.cantidad;
+      const entries: Array<StockEntry> = stock.entries;
+      entries.push(entry);
+      stock.entries = entries;
+      await this.stockRepo.save(stock);
+      return await this.entryRepo.save(entry);
+    } else return null;
+  }
+
+  async softDeleteEntryById(id: number): Promise<Object> {
+    const entryToRemove: StockEntryEntity | null = await this.getEntryById(id);
+    if (entryToRemove) {
+      const stock: StockEntity | null = await this.getStockById(
+        entryToRemove?.stock.stock_id
+      );
+      if (stock) {
+        stock.cantidad = stock.cantidad - entryToRemove.cantidad;
+        await this.stockRepo.save(stock);
+        entryToRemove.soft_delete = true;
+        await this.entryRepo.save(entryToRemove);
+        return { status: "OK" };
+      } else return { error: "entry's stock not found!" };
+    } else return { error: "entry not found!" };
+  }
+
+  async updateEntry(entry: StockEntry): Promise<StockEntryEntity | null> {
+    const oldEntry = await this.getEntryById(entry.entry_id);
+    if (oldEntry) {
+      const oldQuantity: number = oldEntry?.cantidad;
+      const newQuantity: number = entry.cantidad;
+      const stock = await this.getStockById(oldEntry?.stock.stock_id);
+      if (stock) {
+        stock.cantidad = stock.cantidad - oldQuantity;
+        stock.cantidad = stock.cantidad + newQuantity;
+        let entries = stock.entries;
+        entries.push(entry);
+        stock.entries = entries;
+        await this.stockRepo.save(stock);
+        return await this.entryRepo.save(entry);
+      } else return null;
+    } else return null;
+  }
+
+  async getStockById(id: number): Promise<StockEntity | null> {
+    return await this.stockRepo.findOneBy({ stock_id: id, soft_delete: false });
+  }
+}

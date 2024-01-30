737c8984432800584c288620644f348c5e528d69
Leonardo <terlizzileonardo@gmail.com>
2024-01-30 18:58:35 -0300

    now purchases updates stocks

--- Server/src/dto/requests/stock.request.ts
+++ Server/src/dto/requests/stock.request.ts
@@ -4,6 +4,7 @@ import { StockEntryRequest } from "./stockEntry.request";
 export interface StockRequest {
   stock_id?: number;
   product: ProductRequest;
+  product_id: number;
   cantidad: number;
   entries: Array<StockEntryRequest>;
 }

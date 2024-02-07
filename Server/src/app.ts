6259013f7f02701ccfabf617c83e445251ef3c46
Leonardo <terlizzileonardo@gmail.com>
2024-02-07 15:38:54 -0300

    added google oauth 2 login. Added jwt. Added redirect route to client with token as params. This route saves the token on localstorage.

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -1,20 +1,24 @@
 const express = require("express");
 const cors = require("cors");
-const session = require("express-session");
+import passport from "passport";
+import { Strategy as GoogleStrategy, Profile } from "passport-google-oauth20";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 const morgan = require("morgan");
 const helmet = require("helmet");
 const apiRouter = require("./routes/api.router");
 const types = require("pg").types;
-
 import "reflect-metadata";
-import connectPgSimple from "connect-pg-simple";
 import { AppDataSource } from "./data-source";
+import { Request, Response } from "express";
+import { UserService } from "./services/UserService";
+import { User } from "./models/User";
 
 const app = express();
+const userServ = new UserService();
+
 app.use(express.json());
 startDBConnection();
-
-// const pgSession = connectPgSimple(session);
 
 const PORT: number = parseInt(process.env.DB_PORT!) || 5432;
 
@@ -28,26 +32,63 @@ app.use(
 );
 app.use(morgan("combined"));
 
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      callbackURL: "/auth/google/callback",
+    },
+    async (
+      accessToken: string,
+      refreshToken: string,
+      profile: Profile,
+      done
+    ) => {
+      try {
+        //  console.log("DATA FROM GOOGLE: ", profile);
+        const name = profile.name?.givenName;
+        const lastname = profile.name?.familyName;
+        const user = {
+          email: profile.emails![0].value,
+          username: profile.displayName,
+          name: name,
+          lastname: lastname,
+          avatar: profile.photos![0].value,
+          roles: ["USER"],
+        };
+        console.log("User: ", user);
+        const alreadyUser = (await userServ.getUserByEmail(user.email)) as User;
+        if (!alreadyUser) {
+          await userServ.saveUser(user as User);
+        }
+        const token = jwt.sign(user, process.env.JWT_SECRET!);
+        return done(null, token);
+      } catch (err: any) {
+        return done(err);
+      }
+    }
+  )
+);
+
 types.setTypeParser(1700, function (val: any) {
   return parseFloat(val);
 });
 
-// app.use(
-//   session({
-//     store: new pgSession({
-//       conObject: {
-//         user: process.env.DB_USER,
-//         host: process.env.DB_HOST,
-//         database: process.env.DB_DATABASE,
-//         password: process.env.DB_PASSWORD,
-//         port: PORT,
-//       },
-//     }),
-//     secret: "tu-secreto-aqui",
-//     resave: false,
-//     saveUninitialized: true,
-//   })
-// );
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { session: false }),
+  (req: Request, res: Response) => {
+    //const token = jwt.sign({ user: req.user }, process.env.JWT_SECRET!);
+    const url = process.env.DEV_CLIENT_URL;
+    res.redirect(url + `/token?token=${req.user}`);
+  }
+);
 
 app.use("/", apiRouter);
 
---
+++ Web/src/app/token/token.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-token',
+  templateUrl: './token.component.html',
+  styleUrls: ['./token.component.css'],
+})
+export class TokenComponent {
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit(): void {
+    this.route.queryParamMap.subscribe((params) => {
+      console.log(params);
+      const token = params.get('token');
+      localStorage.setItem('token', token as string);
+      this.router.navigate(['/']);
+    });
+  }
+}

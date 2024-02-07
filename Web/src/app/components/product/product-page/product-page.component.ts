cc1ea007e0f7a119d787f1a3073d9733cd7a245c
Leonardo <terlizzileonardo@gmail.com>
2024-02-07 11:30:00 -0300

    fixed 'addToCart' btn on product-page component.

--- Web/src/app/components/product/product-page/product-page.component.ts
+++ Web/src/app/components/product/product-page/product-page.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 import { StockService } from 'src/app/services/stock.service';
+import { Cart } from 'src/common/models/cart';
 import { Product } from 'src/common/models/product';
 
 @Component({
@@ -18,7 +20,9 @@ export class ProductPageComponent {
   constructor(
     private prodServ: ProductService,
     private route: ActivatedRoute,
-    private stockServ: StockService
+    private stockServ: StockService,
+    private cartServ: CartService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -52,5 +56,27 @@ export class ProductPageComponent {
         console.log(data);
         this.isStock = data.stock;
       });
+  }
+
+  addToCart() {
+    console.log(this.product);
+    if (this.product) {
+      const cart = {} as Cart;
+      cart.cantidad = 1;
+      cart.product = this.product;
+      cart.user_id = 1; //TODO
+      cart.total = this.product.price * cart.cantidad;
+      if (localStorage.getItem('carts')) {
+        const carts: Array<Cart> = JSON.parse(
+          localStorage.getItem('carts') as string
+        );
+        carts.push(cart);
+        localStorage.removeItem('carts');
+        localStorage.setItem('carts', JSON.stringify(carts));
+      }
+      this.cartServ.create(cart).subscribe((cartResponse: any) => {
+        this.router.navigate(['/purchase']);
+      });
+    }
   }
 }

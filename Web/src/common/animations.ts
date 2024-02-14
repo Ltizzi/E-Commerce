d2bac277d7596da04471598f014d615312623fae
Leonardo <terlizzileonardo@gmail.com>
2024-02-14 17:51:43 -0300

    added animations a product card, navbar, product list,components. Changed type's table in admin dashboard to animate rows whenever the user update/create/delete a tye. Added new animations to the animations.ts file.

--- Web/src/common/animations.ts
+++ Web/src/common/animations.ts
@@ -23,3 +23,31 @@ export const inAndOutAnimation = trigger('in&Out', [
   ),
   transition('in<=>out', animate(100)),
 ]);
+
+export const hoverInAndOutAnimationNav = trigger('hover&leave', [
+  state(
+    'hover',
+    style({
+      // transform: 'scale(1.1)',
+      backgroundColor: '#818cf8',
+      color: 'white',
+      borderRadius: '0.7rem',
+      padding: '0.5rem 0.7rem',
+      opacity: 0.9,
+    })
+  ),
+  state('leave', style({ transform: 'scale(1)' })),
+  transition('hover<=>leave', animate(150)),
+]);
+
+export const fadeIndAndFadeOutAnimation = trigger('fadeIn&Out', [
+  state(
+    'in',
+    style({
+      opacity: 1,
+      transform: 'scale(1)',
+    })
+  ),
+  state('out', style({ opacity: 0, transform: 'scale(0.75)' })),
+  transition('in<=>out', animate(200)),
+]);

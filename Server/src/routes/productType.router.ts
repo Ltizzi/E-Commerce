0cc6d929d67227e19e7159c44b90ef35207566d3
Leonardo <terlizzileonardo@gmail.com>
2024-02-16 16:59:09 -0300

    added JWTUserInfo interface. Added admin checker middleware. Added authentication -check jwt and check role- middleware to all routes. Fixed jwt always saving default roles to token -now will fetch roles from db if the user already exists-.

--- Server/src/routes/productType.router.ts
+++ Server/src/routes/productType.router.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { ProductTypeController } from "../controllers/productType.controller";
+import { authenticateJWT, isAdmin } from "../utils/authMiddleware";
 
 const productTypeRouter: Router = Router();
 const productTypeController = new ProductTypeController();
@@ -7,16 +8,26 @@ const productTypeController = new ProductTypeController();
 productTypeRouter.get("/all", productTypeController.httpGetAllProductTypes);
 productTypeRouter.get(
   "/withPagination",
+  isAdmin,
   productTypeController.httpGetTypesWithPagination
 );
 productTypeRouter.get("/byId", productTypeController.httpGetTypeById);
 productTypeRouter.get("/byName", productTypeController.httpGetTypeByName);
-productTypeRouter.get("/count", productTypeController.httpCountTypes);
-productTypeRouter.post("/new", productTypeController.httpCreateProductType);
+productTypeRouter.get("/count", isAdmin, productTypeController.httpCountTypes);
+productTypeRouter.post(
+  "/new",
+  isAdmin,
+  productTypeController.httpCreateProductType
+);
 productTypeRouter.delete(
   "/delete",
+  isAdmin,
   productTypeController.httpSoftDeleteProductType
 );
-productTypeRouter.patch("/update", productTypeController.httpUpdateProductType);
+productTypeRouter.patch(
+  "/update",
+  isAdmin,
+  productTypeController.httpUpdateProductType
+);
 
 module.exports = productTypeRouter;

5c730efe1752b0a1afe9de7d55ed77a3fe4ea997
Leonardo <terlizzileonardo@gmail.com>
2024-02-20 12:41:36 -0300

    added review service, controller and router.

---
+++ Server/src/routes/review.router.ts
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import { ReviewController } from "../controllers/review.controller";
+
+const reviewRouter: Router = Router();
+const reviewController = new ReviewController();
+
+reviewRouter.get("/all", reviewController.httpGetAllReviews);
+reviewRouter.get(
+  "/withPagination",
+  reviewController.httpGetReviewsWithPagination
+);
+reviewRouter.get("/byId", reviewController.httpGetReviewById);
+reviewRouter.get(
+  "/byUserIdWithPagination",
+  reviewController.httpGetReviewsFromUserWithPagination
+);
+reviewRouter.get(
+  "/byProductIdWithPagination",
+  reviewController.httpGetProductReviewsWithPagination
+);
+reviewRouter.get("/count", reviewController.httpGetTotalReviewsNumber);
+reviewRouter.get("/countByUser", reviewController.httpGetTotalReviewsFromUser);
+reviewRouter.post("/new", reviewController.httpSaveReview);
+reviewRouter.patch("/update", reviewController.httpUpdateReview);
+
+module.exports = reviewRouter;
